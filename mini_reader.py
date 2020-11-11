cd9e4641c4c582aa8b060f0fb7bc45ebd7fcc4c2
SubMax <subbbbbbbbject@gmail.com>
2020-11-11 18:33:00 +0500

    получение html разметки с web-страницы указанной в url

---
+++ mini_reader.py
@@ -0,0 +1,22 @@
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError, URLError
+
+
+class MiniReader:
+    def __init__(self, url):
+        self.request = Request(url)
+        self._get_page()
+
+    def _get_page(self):
+        try:
+            with urlopen(self.request) as response:
+                self.code = response.code
+                self.context_type = response.headers.get('Content-Type')
+                if 'charset=' in self.context_type:
+                    self.charset = self.context_type.split()[-1]
+                    self.charset = self.charset.split('=')[-1]
+                self.page = response.read().decode(self.charset)
+        except HTTPError as err:
+            self.code = err.code
+            self.page = err.reason
+
