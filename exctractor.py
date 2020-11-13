056d1ff649cc16c681b1cf500a83eb8fb5fec74b
SubMax <subbbbbbbbject@gmail.com>
2020-11-13 13:14:50 +0500

    заполнение экземпляров Door, определение начала и конца статьи

--- exctractor.py
+++ exctractor.py
@@ -1,6 +1,6 @@
 from html.parser import HTMLParser
 from abc import ABC
-from settings import ATTR_SCHEME_DICT, TEXT_TAG_DICT
+from settings import ATTR_SCHEME_DICT, TEXT_TAG_DICT, LINK_TAG_DICT
 
 
 class Door:
@@ -12,7 +12,7 @@ class Door:
         self.is_opened = False
 
     def add_data(self, data):
-        self.data += data
+        self.data += data.strip()
         self.is_opened = True
 
     def __repr__(self):
@@ -22,61 +22,82 @@ class Door:
 class ExtractorText(HTMLParser, ABC):
     def __init__(self):
         self.text = ''
-        self.list_tegs = list()
+        self.tag_list = list()
         self.doors = []
-        self.target_tag = ''
         self.is_news = False
         self.is_text = False
+        self.is_link = False
         self.cur_link = ''
         super(ExtractorText, self).__init__()
 
     def handle_starttag(self, tag, attrs):
+        self._is_the_beginning(attrs)
+
+        if self.is_news:
+            if TEXT_TAG_DICT.get(tag, False):
+                self.is_text = True
+                self.tag_list.append(tag)
+                self.doors.append(Door(tag, attrs))
+            if LINK_TAG_DICT.get(tag, False):
+                for attr in attrs:
+                    if attr[0] == 'href':
+                        self.cur_link = attr[1]
+                        self.is_link = True
+
+    def handle_endtag(self, tag):
+        if self.is_news:
+            if TEXT_TAG_DICT.get(tag, False):
+                self.tag_list.pop()
+                for door in self.doors[::-1]:
+                    if door.tag == tag and door.is_open:
+                        door.is_open = False
+                        # if not LINK_TAG_DICT.get(door.tag, False):
+                        #     door.add_data(f'{tag}')
+                        break
+        self._is_the_end()
+
+    def handle_data(self, data):
+        if self.is_text:
+            last_add_tag = self.tag_list[-1]  # последний добавленный тег
+            if TEXT_TAG_DICT.get(last_add_tag, False):
+                for door in self.doors[::-1]:
+                    if door.is_open and door.tag == last_add_tag:
+                        door.add_data(data)
+                        break
+                if LINK_TAG_DICT.get(last_add_tag):
+                    for door in self.doors[::-1]:
+                        if door.is_open and door.tag != last_add_tag:
+                            door.add_data('{link}')
+                            break
+
+    def feed(self, data):
+        super(ExtractorText, self).feed(data)
+        self._format_text()
+        return self.text
+
+    def _is_the_beginning(self, attrs):
+        """
+        Определяем начало статьи с помощью атрибутов тега.
+        Доступные значения хранятся в settings.py.
+        :param attrs:
+        :return:
+        """
         for attr in attrs:
             if ATTR_SCHEME_DICT.get(attr[0], False):
                 scheme = ATTR_SCHEME_DICT.get(attr[0])
                 if scheme.get(attr[1], False):
                     self.is_news = True
 
-        if self.is_news:
-            if TEXT_TAG_DICT.get(tag, False):
-                self.is_text = True
-                self.list_tegs.append(tag)
-                self.doors.append(Door(tag, attrs))
-                for attr in attrs:
-                    if attr[0] == 'href':
-                        self.cur_link = attr[1]
-
-    def handle_endtag(self, tag):
-        if self.is_news:
-            if TEXT_TAG_DICT.get(tag, False):
-                self.list_tegs.pop()
-                for door in self.doors[::-1]:
-                    if door.tag == tag and door.is_open:
-                        door.is_open = False
-                        break
-        if self.is_news == True and len(self.list_tegs) == 0:
+    def _is_the_end(self):
+        """
+        Определяет конец статьи.
+        :return:
+        """
+        if self.is_news == True and len(self.tag_list) == 0:
             self.is_news = False
             self.is_text = False
 
-    def handle_data(self, data):
-        if self.is_text:
-            if TEXT_TAG_DICT.get(self.list_tegs[-1], False):
-                if self.list_tegs[-1] == 'a':
-                    self.text += data + f' [{self.cur_link}] '
-                else:
-                    self.text += data + f' <{self.list_tegs[-1]}> '
-                    self.is_text = False
-                for door in self.doors[::-1]:
-                    if door.is_open and door.tag == self.list_tegs[-1]:
-                        door.add_data(data)
-                        break
-
-    def feed(self, data):
-        super(ExtractorText, self).feed(data)
-        self.format_text()
-        return self.text
-
-    def format_text(self):
+    def _format_text(self):
         line = ''
         output_text = ''
         for word in self.text.split():
