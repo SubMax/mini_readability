1c6d5523183b2f61278ee8353d4d8987a522a65f
SubMax <subbbbbbbbject@gmail.com>
2020-11-13 11:20:17 +0500

    создал файл настроек settings.py

---
+++ settings.py
@@ -0,0 +1,28 @@
+
+ATTR_NAME_DICT = {
+    'itemprop': {
+        'articleBody': 'articleBody',
+        'headline': 'headline',
+        'description': 'description',
+        'alternativeHeadline': 'alternativeHeadline',
+    },
+}
+ATTR_SCHEME_DICT = {
+    'itemtype': {
+        'http://schema.org/NewsArticle': 'http://schema.org/NewsArticle',
+        'http://schema.org/Article': 'http://schema.org/Article',
+    }
+}
+
+TEXT_TAG_DICT = {
+    'h1': 'h1',
+    'p': 'p',
+    'a': 'a',
+    'div': 'div',
+    # 'span': 'span',
+}
+
+TAG_DICT = {
+    'div': 'div',
+    'article': 'article',
+}
