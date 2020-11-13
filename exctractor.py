9c99c37875781570a0983fc3664d317c237902aa
SubMax <subbbbbbbbject@gmail.com>
2020-11-14 01:43:27 +0500

    добавил строки документации

--- exctractor.py
+++ exctractor.py
@@ -6,6 +6,8 @@ import re
 
 
 class Door:
+    """Класс сущность описывающий HTML тег"""
+
     def __init__(self, tag, attrs):
         self.tag = tag
         self.attrs = attrs
@@ -21,7 +23,9 @@ class Door:
         return f'[{self.tag}, {self.attrs}, {self.data}]'
 
 
-class ExtractorText(HTMLParser, ABC):
+class ExtractorText(HTMLParser):
+    """Класс для работы с HTML"""
+
     def __init__(self):
         self.text = ''
         self.tag_list = list()
@@ -33,20 +37,22 @@ class ExtractorText(HTMLParser, ABC):
         super(ExtractorText, self).__init__()
 
     def handle_starttag(self, tag, attrs):
+        """Метод вызывается при появлении открывающегося HTML тега"""
         self._is_the_beginning(attrs)
 
         if self.is_news:
-            if TEXT_TAG_DICT.get(tag, False):
+            if TEXT_TAG_DICT.get(tag, False):  # Валидация имен необходимых тегов
                 self.is_text = True
                 self.tag_list.append(tag)
                 self.doors.append(Door(tag, attrs))
-            if LINK_TAG_DICT.get(tag, False):
+            if LINK_TAG_DICT.get(tag, False):  # Если тег ссылка, сохраняем URL хранящийся в атрибуте
                 for attr in attrs:
                     if attr[0] == 'href':
                         self.cur_link = attr[1]
                         self.is_link = True
 
     def handle_endtag(self, tag):
+        """Метод вызывается при закрытии HTML тега"""
         if self.is_news:
             if TEXT_TAG_DICT.get(tag, False):
                 self.tag_list.pop()
@@ -57,20 +63,22 @@ class ExtractorText(HTMLParser, ABC):
         self._is_the_end()
 
     def handle_data(self, data):
+        """Метод вызывается при появлении содержимого HTML тега"""
         if self.is_text:
             last_add_tag = self.tag_list[-1]  # последний добавленный тег
-            if TEXT_TAG_DICT.get(last_add_tag, False):
+            if TEXT_TAG_DICT.get(last_add_tag, False):  # Если тег текстовый
                 for door in self.doors[::-1]:
                     if door.is_open and door.tag == last_add_tag:
                         door.add_data(data)
                         break
-                if LINK_TAG_DICT.get(last_add_tag):
+                if LINK_TAG_DICT.get(last_add_tag):  # Если ссылка
                     for door in self.doors[::-1]:
                         if door.is_open and door.tag != last_add_tag:
                             door.add_data('{link}')
                             break
 
     def feed(self, data):
+        """Метод класса HTMLParser. Дальнейшая обработка полученных данных"""
         super(ExtractorText, self).feed(data)
         self._filter_door()
         self._format_text()
@@ -99,9 +107,11 @@ class ExtractorText(HTMLParser, ABC):
             self.is_text = False
 
     def _filter_door(self):
+        """Метод для работы с экземплярами класса Door, описывающих HTML тег"""
         self.doors = list(filter(lambda door: door.data != '', self.doors))
 
         def contains(door):
+            """Определяет содержат ли атрибуты тега фильтры указанные в FILTERS_CONTAINS"""
             for attr in door.attrs:
                 name_attr = attr[0]
                 value = attr[1]
@@ -111,6 +121,7 @@ class ExtractorText(HTMLParser, ABC):
             return True
 
         def match(door):
+            """Определяет совпадают ли атрибуты тега с фильтрами указанными в FILTERS_MATCH"""
             for attr in door.attrs:
                 name_attr = attr[0]
                 value = attr[1]
@@ -120,16 +131,18 @@ class ExtractorText(HTMLParser, ABC):
             return True
 
         def contains_data(door):
+            """Определяет содержат ли содержимое тега фильтры указанные в FILTERS_CONTAINS_DATA"""
             for fltr in FILTERS_CONTAINS_DATA:
                 if fltr in door.data:
                     return False
             return True
 
-        self.doors = list(filter(contains, self.doors))
+        self.doors = list(filter(contains, self.doors))  # Применение фильтров
         self.doors = list(filter(match, self.doors))
         self.doors = list(filter(contains_data, self.doors))
 
     def _format_text(self):
+        """Сборка текста из экземпляров класса Door и форматирование"""
         output_text = ''
         link = ''
         new_text = ''
@@ -147,7 +160,7 @@ class ExtractorText(HTMLParser, ABC):
             line_count = 0
             new_line = '    '
             for word in paragraph.strip().split():
-                if (len(new_line + word) + 1) // 80 > line_count:
+                if (len(new_line + word) + 1) // 80 > line_count:   # Ширина текста
                     new_line += '\n' + word + ' '
                     line_count += 1
                 else:
