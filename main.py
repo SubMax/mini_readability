61db1186b11a5181b45fadda1a327ed85c5f4af8
SubMax <subbbbbbbbject@gmail.com>
2020-11-14 00:41:00 +0500

    изменил интерфейс командной строки

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from mini_reader import MiniReader
 
 
 if __name__ == '__main__':
-    print("Для выхода введите: q")
+    print("Утилита для сохранения текста статьи сайта в файле.\nДля выхода введите: q")
     while True:
         url = input("Введите url-адрес:")
         if url == 'q':
@@ -10,3 +10,4 @@ if __name__ == '__main__':
             break
         else:
             mr = MiniReader(url=url)
+            print(f'Файл сохранен: {mr.path}\\{mr.file_name}')
