56ca05ada3870c04f64a90146c0aae8402d7c758
SubMax <subbbbbbbbject@gmail.com>
2020-11-14 01:09:23 +0500

    добавил строки документации

--- mini_reader.py
+++ mini_reader.py
@@ -5,7 +5,14 @@ from exctractor import ExtractorText
 
 
 class MiniReader:
+    """
+    Главный класс утилиты.
+    Создание запроса. Выполнение запроса. Сохранение в файл.
+    """
     def __init__(self, url):
+        """
+        :param url: URL страницы
+        """
         self.request = Request(url)
         self.parser = ExtractorText()
         self._get_page()
@@ -13,10 +20,11 @@ class MiniReader:
         self._save_to_file()
 
     def _get_page(self):
+        """Запрос страницы"""
         try:
             with urlopen(self.request) as response:
                 self.code = response.code
-                self.context_type = response.headers.get('Content-Type')
+                self.context_type = response.headers.get('Content-Type')    # извлекакем charset из HTTP заголовка
                 if 'charset=' in self.context_type:
                     self.charset = self.context_type.split()[-1]
                     self.charset = self.charset.split('=')[-1]
@@ -26,6 +34,7 @@ class MiniReader:
             self.page = err.reason
 
     def _save_to_file(self):
+        """Сохранение в файл. Путь формируется автоматически исходя из URL"""
         self.BASE_DIR, _ = path.split(path.abspath(__file__))
         self.path, self.file_name = path.split(path.normpath(self.request.selector))
         self.file_name, _ = path.splitext(self.file_name)
@@ -38,4 +47,5 @@ class MiniReader:
             file.write(self.page)
 
     def _extract_text(self):
+        """Извлечение текста статьи из html страницы"""
         self.page = self.parser.feed(self.page)
